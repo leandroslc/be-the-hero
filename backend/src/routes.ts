70d6bcd72f1076786288f766e7ab485e5356c3da
Leandro Silva Luz do Carmo <leandro_scarmo@hotmail.com>
2020-03-29 20:51:12 -0300

    feat(backend): Using regex validation for whatsapp phone number in ngos creation

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -21,7 +21,7 @@ routes.post('/ngos', celebrate({
   [Segments.BODY]: Joi.object().keys({
     name: Joi.string().required(),
     email: Joi.string().required().email(),
-    whatsapp: Joi.number().required().min(11),
+    whatsapp: Joi.string().required().regex(/[0-9\-+\s]+/),
     city: Joi.string().required(),
     uf: Joi.string().required().length(2),
   }),
