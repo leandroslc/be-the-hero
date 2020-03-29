0bfd1ac328017b73c4945955c99ecb35f4e462e1
Leandro Silva Luz do Carmo <leandro_scarmo@hotmail.com>
2020-03-29 18:49:08 -0300

    feat: Backend routes validation

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,4 +1,7 @@
 import { Router } from 'express';
+import { celebrate, Segments } from 'celebrate';
+import Joi from '@hapi/joi';
+
 import NGOController from './controllers/NGOController';
 import IncidentController from './controllers/IncidentController';
 import ProfileController from './controllers/ProfileController';
@@ -6,15 +9,54 @@ import SessionController from './controllers/SessionController';
 
 const routes = Router();
 
-routes.post('/sessions', SessionController.create);
+routes.post('/sessions', celebrate({
+  [Segments.BODY]: Joi.object().keys({
+    id: Joi.string().required(),
+  }),
+}), SessionController.create);
 
 routes.get('/ngos', NGOController.index);
-routes.post('/ngos', NGOController.create);
 
-routes.get('/profile', ProfileController.index);
+routes.post('/ngos', celebrate({
+  [Segments.BODY]: Joi.object().keys({
+    name: Joi.string().required(),
+    email: Joi.string().required().email(),
+    whatsapp: Joi.number().required().min(11),
+    city: Joi.string().required(),
+    uf: Joi.string().required().length(2),
+  }),
+}), NGOController.create);
 
-routes.get('/incidents', IncidentController.index);
-routes.post('/incidents', IncidentController.create);
-routes.delete('/incidents/:id', IncidentController.delete);
+routes.get('/profile', celebrate({
+  [Segments.HEADERS]: Joi.object({
+    authorization: Joi.string().required(),
+  }).unknown(),
+}), ProfileController.index);
+
+routes.get('/incidents', celebrate({
+  [Segments.QUERY]: Joi.object().keys({
+    page: Joi.number(),
+  }),
+}), IncidentController.index);
+
+routes.post('/incidents', celebrate({
+  [Segments.BODY]: Joi.object().keys({
+    title: Joi.string().required(),
+    description: Joi.string().required(),
+    value: Joi.number().required(),
+  }),
+  [Segments.HEADERS]: Joi.object({
+    authorization: Joi.string().required(),
+  }).unknown(),
+}), IncidentController.create);
+
+routes.delete('/incidents/:id', celebrate({
+  [Segments.PARAMS]: Joi.object().keys({
+    id: Joi.number().required(),
+  }),
+  [Segments.HEADERS]: Joi.object({
+    authorization: Joi.string().required(),
+  }).unknown(),
+}), IncidentController.delete);
 
 export default routes;
