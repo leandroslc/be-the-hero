1e9fe997eb51d90583cfa47cdba3246b0ba719ba
Leandro Silva Luz do Carmo <leandro_scarmo@hotmail.com>
2020-03-30 00:38:03 -0300

    feat(mobile): Added .env configuration for storing the dev IP

--- mobile/src/services/api.ts
+++ mobile/src/services/api.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
+import { IP } from 'react-native-dotenv'
 
 const api = axios.create({
-  baseURL: 'http://192.168.15.51:3333',
+  baseURL: `http://${IP}:3333`,
 });
 
 export default api;
